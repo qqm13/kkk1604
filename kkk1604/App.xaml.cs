d2a1530c88221367aafde191ecbc12c307f11337
Max <Max@DESKTOP-A80Q5VJ>
2025-04-16 03:22:39 +1000

    Добавьте файлы проекта.

---
+++ kkk1604/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace kkk1604
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
