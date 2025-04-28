8dc188e8cd421c74a2d715619b4894fa43bb6218
Max <fbstars666@gmail.com>
2025-04-28 20:32:12 +1000

    vm делать меню и окно

--- kkk1604/Model/Db/OrganizationsDB.cs
+++ kkk1604/Model/Db/OrganizationsDB.cs
@@ -63,120 +63,160 @@ namespace kkk1604.Model.Db
             return result;
         }
 
-        //public List<DeathPlace> SelectAll()
-        //{
-        //    List<DeathPlace> deathPlaces = new List<DeathPlace>();
-        //    if (connection == null)
-        //        return deathPlaces;
+        public List<Organization> SelectAll()
+        {
+            List<Organization> organizations = new List<Organization>();
+            if (connection == null)
+                return organizations;
 
 
-        //    if (connection.OpenConnection())
-        //    {
-        //        var command = connection.CreateCommand("SELECT dp.id, dp.Title, dp.GraveTypeId, dp.CoffinTypeId, dp.Price, dp.FlowersId, gt.Title, gt.Price, ct.Title, ct.Price, f.Title,f.Price  FROM DeathPlace dp JOIN CoffinType ct ON dp.CoffinTypeId = ct.id JOIN GraveType gt ON dp.GraveTypeId = gt.id JOIN Flowers f ON dp.FlowersId = f.id");
+            if (connection.OpenConnection())
+            {
+                var command = connection.CreateCommand("SELECT o.id, DeathplaceId,GuestCount,Necrology,LastDinner,LastSlideshow,GuestBus,Catafalque,Priest,  date,  PlaceId,  o.Price,  p.CemetaryAdress,  p.CemeterySectorNumber,  p.CemeteryPlotNumber,  dp.Title,  dp.GraveTypeId,  dp.CoffinTypeId,dp.FlowersId,ct.Title,  gt.Title,  f.Title FROM Organization o JOIN Place p ON o.PlaceId = p.id JOIN DeathPlace dp ON o.DeathplaceId = dp.id JOIN CoffinType ct ON dp.CoffinTypeId = ct.id JOIN GraveType gt ON dp.GraveTypeId = gt.id JOIN Flowers f ON dp.FlowersId = f.id");
 
-        //        try
-        //        {
-        //            MySqlDataReader dr = command.ExecuteReader();
-        //            while (dr.Read())
-        //            {
-        //                int id = dr.GetInt32(0);
-        //                string title = string.Empty;
-        //                if (!dr.IsDBNull(1))
-        //                    title = dr.GetString(1);
-        //                int graveTypeId = dr.GetInt32(2);
-        //                int coffinTypeId = dr.GetInt32(3);
-        //                int price = dr.GetInt32(4);
-        //                int flowersId = dr.GetInt32(5);
-
-        //                string gravetitle = string.Empty;
-        //                if (!dr.IsDBNull(6))
-        //                    gravetitle = dr.GetString(6);
-        //                int graveprice = dr.GetInt32(7);
-
-        //                string coffintitle = string.Empty;
-        //                if (!dr.IsDBNull(8))
-        //                    coffintitle = dr.GetString(8);
-        //                int coffinprice = dr.GetInt32(9);
-
-        //                string flowerstitle = string.Empty;
-        //                if (!dr.IsDBNull(10))
-        //                    flowerstitle = dr.GetString(10);
-        //                int flowerprice = dr.GetInt32(11);
-
-
-        //                Grave DeathPlaceGrave = new Grave
-        //                {
-        //                    Id = graveTypeId,
-        //                    Title = gravetitle,
-        //                    Price = graveprice
-        //                };
-
-        //                Coffin DeathPlaceCoffin = new Coffin
-        //                {
-        //                    Id = coffinTypeId,
-        //                    Title = coffintitle,
-        //                    Price = coffinprice
-        //                };
-
-        //                Flower DeathPlaceFlower = new Flower
-        //                {
-        //                    Id = flowersId,
-        //                    Title = flowerstitle,
-        //                    Price = flowerprice
-        //                };
-
-        //                deathPlaces.Add(new DeathPlace
-        //                {
-        //                    Id = id,
-        //                    Title = title,
-        //                    GraveTypeId = graveTypeId,
-        //                    CoffinTypeId = graveTypeId,
-        //                    FlowersId = flowersId,
-        //                    Price = price,
-
-        //                    Coffin = DeathPlaceCoffin,
-        //                    Grave = DeathPlaceGrave,
-        //                    Flower = DeathPlaceFlower
-
-        //                });
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            MessageBox.Show(ex.Message);
-        //        }
-        //    }
-        //    connection.CloseConnection();
-        //    return deathPlaces;
-        //}
-
-        //public bool Update(DeathPlace edit)
-        //{
-        //    bool result = false;
-        //    if (connection == null)
-        //        return result; if (connection.OpenConnection())
-        //    {
-        //        var mc = connection.CreateCommand($"update `DeathPlace` set `Title`=@title, `GraveTypeId`=@graveTypeId, `CoffinTypeId`=@coffinTypeId, `Price`=@price, `FlowersId`=@flowersId where `id` = {edit.Id}");
-        //        mc.Parameters.Add(new MySqlParameter("title", edit.Title));
-        //        mc.Parameters.Add(new MySqlParameter("graveTypeId", edit.Grave.Id));
-        //        mc.Parameters.Add(new MySqlParameter("coffinTypeId", edit.Coffin.Id));
-        //        mc.Parameters.Add(new MySqlParameter("price", edit.Price));
-        //        mc.Parameters.Add(new MySqlParameter("flowersId", edit.Flower.Id));
+                try
+                {
+                    MySqlDataReader dr = command.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        int id = dr.GetInt32(0);
+                        int deathplaceId = dr.GetInt32(1);
+                        int guestCount = dr.GetInt32(2);
+                        bool necrology = dr.GetBoolean(3);
+                        bool lastDiner = dr.GetBoolean(4);
+                        bool lastSlideShow = dr.GetBoolean(5);
+                        bool guestBus = dr.GetBoolean(6);
+                        bool catafalque = dr.GetBoolean(7);
+                        bool priest = dr.GetBoolean(8);
+                        DateOnly dateEvent = new DateOnly();
+                          dateEvent = dr.GetDateOnly(9);
+                        int placeId = dr.GetInt32(10);
+                        int price = dr.GetInt32(11);
+                        string cemetaryAdress = string.Empty;
+                        if (!dr.IsDBNull(12))
+                            cemetaryAdress = dr.GetString(12);
+                        int cemeterySectorNumber = dr.GetInt32(13);
+                        int cemeteryPlotNumber = dr.GetInt32(14);
+                        string deathPlaceTitle = string.Empty;
+                        if (!dr.IsDBNull(15))
+                            deathPlaceTitle = dr.GetString(15);
+                        int graveId = dr.GetInt32(16);
+                        int coffinid = dr.GetInt32(17);
+                        int flowerId = dr.GetInt32(18);
+                        string coffinTitle = string.Empty;
+                        if (!dr.IsDBNull(19))
+                            coffinTitle = dr.GetString(19);
+                        string graveTitle = string.Empty;
+                        if (!dr.IsDBNull(20))
+                            graveTitle = dr.GetString(20); 
+                        string flowerTitle = string.Empty;
+                        if (!dr.IsDBNull(21))
+                            flowerTitle = dr.GetString(21);
 
 
-        //        try
-        //        {
-        //            mc.ExecuteNonQuery();
-        //            result = true;
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            MessageBox.Show(ex.Message);
-        //        }
-        //    }
-        //    connection.CloseConnection();
-        //    return result;
-        //}
+                        Grave organizationGrave = new Grave
+                        {
+                            Id = graveId,
+                            Title = graveTitle
+                        };
+
+                        Coffin organizationCoffin = new Coffin
+                        {
+                            Id = coffinid,
+                            Title = flowerTitle
+
+                        };
+
+                        Flower organizationFlower = new Flower
+                        {
+                            Id = flowerId,
+                            Title = flowerTitle
+                        };
+
+                        Place organizationPlace = new Place
+                        {
+                            Id = placeId,
+                            CemetaryAdress = cemetaryAdress,
+                            CemeteryPlotNumber = cemeteryPlotNumber,
+                            CemeterySectorNumber = cemeterySectorNumber
+                        };
+
+                        DeathPlace organizationDeathPlace = new DeathPlace
+                        {
+                            Id = deathplaceId,
+                            CoffinTypeId = coffinid,
+                            GraveTypeId = graveId,
+                            FlowersId = flowerId,
+
+                            Coffin = organizationCoffin,
+                            Grave = organizationGrave,
+                            Flower = organizationFlower
+                        };
+
+                        organizations.Add(new Organization
+                        {
+                            Id = id,
+                            DeathPlaceId = deathplaceId,
+                            DeathPlace = organizationDeathPlace,
+                            GuestCount = guestCount,
+                            Necrology = necrology,
+                            LastDinner = lastDiner,
+                            LastSlideshow = lastSlideShow,
+                            GuestBus = guestBus,
+                            Catafalque = catafalque,
+                            Priest = priest,
+                            Date = dateEvent,
+                            PlaceId = placeId,
+                            Place = organizationPlace,
+                            Price = price,
+
+
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            connection.CloseConnection();
+            return organizations;
+        }
+
+        public bool Update(Organization edit)
+        {
+            bool result = false;
+            if (connection == null)
+                return result; if (connection.OpenConnection())
+            {
+                var mc = connection.CreateCommand($"update `Organization` set `DeathplaceId`=@DeathplaceId, `GuestCount`=@GuestCount, `Necrology`=@Necrology, `LastDinner`=@LastDinner, `LastSlideshow`=@LastSlideshow,`GuestBus`=@GuestBus,`Catafalque`=@Catafalque,`Priest`=@Priest,`Date`=@Date,`PlaceId`=@PlaceId, `Price`=@Price where `id` = {edit.Id}");
+                mc.Parameters.Add(new MySqlParameter("DeathplaceId", edit.DeathPlace.Id));
+                mc.Parameters.Add(new MySqlParameter("GuestCount", edit.GuestCount));
+                mc.Parameters.Add(new MySqlParameter("Necrology", edit.Necrology));
+                mc.Parameters.Add(new MySqlParameter("LastDinner", edit.LastDinner));
+                mc.Parameters.Add(new MySqlParameter("LastSlideshow", edit.LastSlideshow));
+                mc.Parameters.Add(new MySqlParameter("GuestBus", edit.GuestBus));
+                mc.Parameters.Add(new MySqlParameter("Catafalque", edit.Catafalque));
+                mc.Parameters.Add(new MySqlParameter("Priest", edit.Priest));
+                mc.Parameters.Add(new MySqlParameter("Date", edit.Date));
+                mc.Parameters.Add(new MySqlParameter("PlaceId", edit.Place.Id));
+                mc.Parameters.Add(new MySqlParameter("Price", edit.Price));
+
+
+
+                try
+                {
+                    mc.ExecuteNonQuery();
+                    result = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            connection.CloseConnection();
+            return result;
+        }
 
 
         public bool Remove(Organization remove)
