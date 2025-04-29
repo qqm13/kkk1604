59066a73ccbdbfe13ba643bdc02aa1430aa8f50e
Max <fbstars666@gmail.com>
2025-04-30 08:19:34 +1000

    сделать отчет лсит2 и добавление и редктирование событий и проверки на все кнопки

--- kkk1604/ViewModel/MainVM.cs
+++ kkk1604/ViewModel/MainVM.cs
@@ -1,10 +1,12 @@
 ﻿using kkk1604.Model;
 using kkk1604.Model.Db;
 using kkk1604.View;
+using Microsoft.Win32;
 using Spire.Xls;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -81,42 +83,54 @@ namespace kkk1604.ViewModel
             {
                 Workbook workbook = new Workbook();
                 //1лист
-                Worksheet sheet = workbook.Worksheets[0];                
+                Worksheet sheet = workbook.Worksheets[0];
+                sheet.Name = "События";
+
                 sheet.Range["A1"].Text = "Количество гостей";
                 sheet.Range["A1"].ColumnWidth = 18;
+                sheet.Range["A1"].BorderAround();
 
                 sheet.Range["B1"].Text = "Дата";
                 sheet.Range["B1"].ColumnWidth = 15;
+                sheet.Range["B1"].BorderAround();
 
                 sheet.Range["C1"].Text = "Адрес";
                 sheet.Range["C1"].ColumnWidth = 15;
+                sheet.Range["C1"].BorderAround();
 
                 sheet.Range["D1"].Text = "Статус";
                 sheet.Range["D1"].ColumnWidth = 15;
+                sheet.Range["D1"].BorderAround();
 
                 sheet.Range["E1"].Text = "Общая цена";
                 sheet.Range["E1"].ColumnWidth = 15;
+                sheet.Range["E1"].BorderAround();
 
                 sheet.Range["F1"].Text = "Тип организации захоронения";
                 sheet.Range["F1"].ColumnWidth = 15;
+                sheet.Range["F1"].BorderAround();
 
                 sheet.Range["G1"].Text = "Тип памятника";
                 sheet.Range["G1"].ColumnWidth = 15;
+                sheet.Range["G1"].BorderAround();
 
                 sheet.Range["H1"].Text = "Тип гроба";
                 sheet.Range["H1"].ColumnWidth = 15;
+                sheet.Range["H1"].BorderAround();
 
                 sheet.Range["I1"].Text = "Тип цветов";
                 sheet.Range["I1"].ColumnWidth = 15;
+                sheet.Range["I1"].BorderAround();
 
                 sheet.Range["L1"].Text = "Количество проведенных событий";
                 sheet.Range["L1"].ColumnWidth = 25;
                 sheet.Range["L1"].IsWrapText = true;
+                sheet.Range["L1"].BorderAround();
 
                 sheet.Range["M1"].Text = "Заработок за все проведенные события"; 
                 sheet.Range["M1"].ColumnWidth = 25;
                 sheet.Range["M1"].IsWrapText = true;
-
+                sheet.Range["M1"].BorderAround();
 
                 int organizationsForReportCount = 0;
                 int organizationsForReportPrice = 0;
@@ -145,6 +159,16 @@ namespace kkk1604.ViewModel
                         sheet.Range[$"H{i + 2}"].Text = organizationsForReport[i].DeathPlace.Coffin.Title.ToString();
                         sheet.Range[$"I{i + 2}"].Text = organizationsForReport[i].DeathPlace.Flower.Title.ToString();
 
+                        sheet.Range[$"A{i + 2}"].BorderAround();
+                        sheet.Range[$"B{i + 2}"].BorderAround();
+                        sheet.Range[$"C{i + 2}"].BorderAround();
+                        sheet.Range[$"D{i + 2}"].BorderAround();
+                        sheet.Range[$"E{i + 2}"].BorderAround();
+                        sheet.Range[$"F{i + 2}"].BorderAround();
+                        sheet.Range[$"G{i + 2}"].BorderAround();
+                        sheet.Range[$"H{i + 2}"].BorderAround();
+                        sheet.Range[$"I{i + 2}"].BorderAround();
+
                         if (organizationsForReport[i].Status == true)
                         {
                             organizationsForReportCount++;
@@ -156,16 +180,23 @@ namespace kkk1604.ViewModel
                 sheet.Range["L2"].Text = organizationsForReportCount.ToString();
                 sheet.Range["L2"].ColumnWidth = 25;
                 sheet.Range["L2"].IsWrapText = true;
+                sheet.Range["L2"].BorderAround();
 
                 sheet.Range["M2"].Text = organizationsForReportPrice.ToString();
                 sheet.Range["M2"].ColumnWidth = 25;
                 sheet.Range["M2"].IsWrapText = true;
+                sheet.Range["M2"].BorderAround();
 
+                var path = String.Format("Reports\\NewReport.xls", AppDomain.CurrentDomain);
 
-                workbook.SaveToFile("C:\\Users\\Max\\source\\repos\\kkk1604\\kkk1604\\Reports\\NewReport.xls");
+                workbook.SaveToFile(path);
 
-               
-
+                try
+                {
+                    //    System.Diagnostics.Process.Start(path);
+                    Process.Start(new ProcessStartInfo(path) { UseShellExecute = true });
+                }
+                catch { }
 
 
                 //делать норм создание очтета летсгоу
